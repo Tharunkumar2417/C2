220a08da53025d699e75bd856b9e672f9254e1a8
Tharun <tharun@Tharuns-MacBook-Air.local>
2025-11-20 23:23:28 +0530

    Tria Calc

---
+++ Practice1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+public class Practice1 {
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int a, b, c, s;
+	System.out.println("Enter the 3 values of the tri to get the area: ");
+	a = sc.nextInt();
+	b = sc.nextInt();
+	c = sc.nextInt();
+	s = (a + b + c) / 2;
+	float area = (float)Math.sqrt(s*(s - a) + (s - b) + (s - c));
+	System.out.println(area);
+}
+
+
+}
