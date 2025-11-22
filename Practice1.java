c8e5881e3914932ec260258cfd846546f6d4c84c
Tharun <tharun@Tharuns-MacBook-Air.local>
2025-11-22 21:49:59 +0530

    input msgs added

--- Practice1.java
+++ Practice1.java
@@ -4,8 +4,11 @@ public class Practice1 {
 	Scanner sc = new Scanner(System.in);
 	int a, b, c, s;
 	System.out.println("Enter the 3 values of the tri to get the area: ");
+	System.out.println("Enter the value of A: ");
 	a = sc.nextInt();
+	System.out.println("Enter the value of B: ");
 	b = sc.nextInt();
+	System.out.println("Enter the value of C: ");
 	c = sc.nextInt();
 	s = (a + b + c) / 2;
 	float area = (float)Math.sqrt(s*(s - a) + (s - b) + (s - c));
